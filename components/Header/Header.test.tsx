56c3134a6b23f67a69b1324434ae540dd78127c6
Pablo <pablobombonato14@gmail.com>
2023-03-31 15:57:10 -0300

    Initial Header config

--- components/Header/Header.test.tsx
+++ components/Header/Header.test.tsx
@@ -2,11 +2,22 @@ import Header from "./";
 import { render, screen } from "@testing-library/react";
 
 describe("Header", () => {
-  it("should render", () => {
+  it("should render the correct title", () => {
+    render(<Header />);
+    const title = screen.getByText("Pablo Bombonato");
+    expect(title).toBeInTheDocument();
+  });
+
+  it("should render the correct links", () => {
     render(<Header />);
 
-    const text = screen.getByText("Header");
-    
-    expect(text).toBeInTheDocument();
+    const solutionsLink = screen.getByText("Soluções");
+    expect(solutionsLink).toBeInTheDocument();
+
+    const projectsLink = screen.getByText("Projetos");
+    expect(projectsLink).toBeInTheDocument();
+
+    const letsTalkLink = screen.getByText("Vamos conversar");
+    expect(letsTalkLink).toBeInTheDocument();
   });
 });
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,8 +1,17 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
-    <header>
+    <header className="flex justify-between items-center w-screen p-4 weight font-black">
       <div>
-        <h1>Header</h1>
+        <Link href="/">
+          <b>Pablo Bombonato</b>
+        </Link>
+      </div>
+      <div className="grid grid-cols-3 text-slate-600">
+        <Link href="/about">Soluções</Link>
+        <Link href="/blog">Projetos</Link>
+        <Link href="/projects" className="text-black">Vamos conversar <span>{">"}</span></Link>
       </div>
     </header>
   );
